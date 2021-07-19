138571f5d26562688fd6d7dafaaa086c743ad220
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2021-07-19 23:24:03 +0200

    Improved jumps.

--- Assets/Scripts/Platformer.cs
+++ Assets/Scripts/Platformer.cs
@@ -16,6 +16,15 @@ public class Platformer : MonoBehaviour
     public float checkGroundRadius;
     public LayerMask groundLayer;
 
+    [SerializeField]
+    float fallMultiplier = 2.5f;
+    [SerializeField]
+    float lowJumpMultiplier = 2f;
+
+    [SerializeField]
+    float rememberGroundedFor;
+    float lastTimeGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +36,7 @@ public class Platformer : MonoBehaviour
     {
         Move();
         Jump();
+        BetterJump();
         CheckIfGrounded();
     }
 
@@ -39,22 +49,38 @@ public class Platformer : MonoBehaviour
     
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
 
+    void BetterJump()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
     void CheckIfGrounded()
     {
-        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
 
-        if(collider != null)
+        if(colliders != null)
         {
             isGrounded = true;
         }
         else
         {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
             isGrounded = false;
         }
     }
