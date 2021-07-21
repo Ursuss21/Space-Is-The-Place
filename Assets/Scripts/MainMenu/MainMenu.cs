cb8eb5c331d82a724e487e0aad57469500b799cb
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2021-07-22 01:36:35 +0200

    Add main menu scene. Add fonts.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MainMenu instance { get; set; }
+
+    [SerializeField]
+    private GameObject loadingScreen = null;
+    [SerializeField] 
+    private Slider slider = null;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RunGame()
     {
-        
+        StartCoroutine(LoadAsync("GameScene"));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadAsync(string name)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
 }
